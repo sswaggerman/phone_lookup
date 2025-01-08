78bff4b5bedd261aa33d76dfa126ec62625ef189
sswaggerman <sswaggermanofficial@gmail.com>
2025-01-08 14:43:18 -0300

    Update phone_lookup.py

--- phone_lookup.py
+++ phone_lookup.py
@@ -1,7 +1,7 @@
 from bs4 import BeautifulSoup as htmlparser
 import requests
 
-def lookup(phone_number):
+def lookup(+542995855346):
     http = requests.get(f"https://free-lookup.net/{phone_number}")
     html = htmlparser(http.text, "html.parser")
     infos = html.findChild("ul", {"class": "report-summary__list"}).findAll("div")
